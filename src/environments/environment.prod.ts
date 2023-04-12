e6ec97eba7f0f0368af225aba7111609bc558a1e
jmccreedy65 <jmccreedy65@gmail.com>
2023-04-12 10:11:48 +0100

    changing back to original user pool

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   cognito: {
-    userPoolId: 'eu-west-2_lnMahHWNh',
-    userPoolWebClientId: 't0nfpg0vvdl58huobic82q0dd',
+    userPoolId: 'eu-west-1_n6JOwSsLd',
+    userPoolWebClientId: '7fq6ea6kv48h6ciubkftu9los1',
   }
 };
